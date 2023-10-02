c49c8189c8fc6848f4a503169709435dc9f56b9e
som <someshbasak2016@gmail.com>
2023-10-02 19:49:38 +0530

    Final commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+API_KEY = "Ngt6d9k6dY2f3BZRDdpgGzgClzjiwsFbQ42Aqfnv"
+url = "https://api.nasa.gov/planetary/apod?" \
+       f"api_key={API_KEY}"
+
+response1 = requests.get(url)
+data = response1.json()
+
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+image_filepath = "image.png"
+
+response2 = requests.get(image_url)
+with open(image_filepath, "wb") as file:
+       file.write(response2.content)
+
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
